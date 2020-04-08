2a9b0f80d05924d18989a16f0607324b2209b5c8
mriganka-d5-studios <mriganka.office55@gmail.com>
2020-04-08 14:56:11 +0530

    update

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import './Card.css';
 
 class Card extends React.Component {
-
+    
     render(){
+
+        const {name, email,id}= this.props;
         return(
-                <h1>Hello world</h1>
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+                <div className='img1'><img  alt='robots' src={`https://robohash.org/${id}test?100x100`}/></div>
+                <div>
 
-
+                 <h2>{name }</h2>
+                <p>{email }</p>
+                </div>
+            </div>
         );
 
     }
---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots=[
+{
+    id:1,
+    name: 'Ghanendra Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:2,
+    name: 'Roma Sonowal',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:3,
+    name: 'B K Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:4,
+    name: 'Rajita Devi',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:5,
+    name: 'Mriganka Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:6,
+    name: 'Rinki Hazarika',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:7,
+    name: 'Kabir Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:8,
+    name: 'Tapan Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:9,
+    name: 'Kashyap Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+{
+    id:10,
+    name: 'Bishal Nath',
+    username: 'ginni',
+    email: 'gkn@gmail.com'
+},
+]
