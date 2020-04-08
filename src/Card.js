fba25df36cc98f741dcfcb665eccd13cc4f5861b
mriganka-d5-studios <mriganka.office55@gmail.com>
2020-04-08 13:58:29 +0530

    new files

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Card extends React.Component {
+
+    render(){
+        return(
+                <h1>Hello world</h1>
+
+
+        );
+
+    }
+}
+
+export default Card;
